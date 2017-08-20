f99f37cd07b8131098470b42540b5380f855c46d
asht123dd <30960101+asht123dd@users.noreply.github.com>
2017-08-21 00:06:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ var htmlTemplate=`<html>
 return htmlTemplate;
 }
 app.get('/', function (req, res) {
-  res.sendFile(path.join(_dirname,'ui','index.html'));
+  res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 app.get('/:articleName',function(req,res)
 //articleName==article-one
