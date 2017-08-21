bdebd8f46aa33d8436fec2da3d8d46f27d24e796
asht123dd <30960101+asht123dd@users.noreply.github.com>
2017-08-22 02:23:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element=document.getElementById('main-text');
 element.innerHTML='New value';
 //Move the image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function marginRight()
+{
+        marginLeft+=10;
+        img.style.marginLeft+'px';
+}
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    var interval=setInterval(movRight,100);
 };
