26e6d1b578e9c3408a4b1e7851f12e4ea0f9d3e9
asht123dd <30960101+asht123dd@users.noreply.github.com>
2017-09-02 00:19:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,9 +110,9 @@ app.get('/ui/madi.png', function (req, res) {
  
 });
  var names=[];
-  app.get('/submit-name/:name',function(req,res){
+  app.get('/submit-name',function(req,res){//URL: /submit-name?name=?????
       //Get the name from the request
-      var name=req.params.name;
+      var name=req.query.name;
       names.push(name);
       //JSON:Javascript Object Notation
       res.send(JSON.stringify(names));//TODO
