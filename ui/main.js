91d3350b2c5ad2df597c395b374c8ad1256eb583
asht123dd <30960101+asht123dd@users.noreply.github.com>
2017-09-02 23:47:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -95,3 +95,38 @@ var name=nameInput.value;
     request.send(null);
     
 };
+var submit3=document.getElementById('submit_btn3');
+submit3.onclick=function(){
+    //Make a request to the server and send a comment
+//Capture a lsit of comments and render it as a list.
+var request =new XMLHttpRequest();
+    //Capture the response and store it in a variable
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            //Take some action
+            if(request.status===200)
+            {
+     //Capture a list of names and render it as a list
+                var comments=request.responseText;
+                comments=JSON.parse(comments);
+                 var list='';
+                 for(var i=0;i<comments.length;i++)
+                 {
+                     list+='<li>'+comments[i]+'</li>';
+                 }
+                var ul=document.getElementById('commentlist');
+                ul.innerHTML=list;
+                
+            }
+        }
+        //Not done yet
+};
+    
+   //Make the request
+   var nameInput=document.getElementById('comment');
+var name=nameInput.value;
+    request.open('GET','http://asht123dd.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+    
+};
